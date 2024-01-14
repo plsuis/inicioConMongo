6947994559f3bc99815db8b5fc2952ef5548b405
suplis <israel.surribas.planas@gmail.com>
2024-01-14 22:12:05 +0100

    Incorporo a función no cliente para o borrado do usuario
    Está na carpeta de funcións.

--- funciones/index.js
+++ funciones/index.js
@@ -1,8 +1,10 @@
 const {insertarUsuario} = require("./insertoDatos.js");
 const {leoDatos} = require("./leoDatos.js")
 const {updateUsuario} = require("./modificoDato.js")
+const {deleteUsuario} = require("./borroDato.js")
 module.exports = {
     insertarUsuario,
     leoDatos,
-    updateUsuario
+    updateUsuario,
+    deleteUsuario
 }
