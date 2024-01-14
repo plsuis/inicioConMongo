7ac66dd6537106662082662837181aeb12049b80
suplis <israel.surribas.planas@gmail.com>
2024-01-14 22:07:51 +0100

    Incorporo a función de borrao o usuario
    > O arquivo chámase borroDato.js

---
+++ funciones/borroDato.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectId} = require("mongodb")
+const url = "mongodb://127.0.0.1:27017";
+const database = 'proba1'
+const client = new MongoClient(url)
+const coleccion = "usuarios";
+
+async function deleteUsuario(id) {
+    try {
+      console.log('Estou en updateUsuario: ',id)
+      await client.connect();
+        const db = client.db(database);
+        const coll = db.collection(coleccion);
+        const dato ={
+            _id:new ObjectId(id)
+        }
+        
+       
+        const result = await coll.deleteOne(dato);
+      console.log(result)
+    } finally {
+      await client.close();
+     
+    }
+  }
+
+  module.exports = {
+    deleteUsuario
+  }
